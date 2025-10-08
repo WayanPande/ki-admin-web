a01122dcdefaf26c3b80a3d9444a782a5027ad74
Wayan Pande <wayan.yudha@amartek.id>
2025-10-08 13:27:17 +0700

    feat: dynamic import dashboard

--- apps/web/src/routes/_auth/_layout/dashboard.tsx
+++ apps/web/src/routes/_auth/_layout/dashboard.tsx
@@ -2,10 +2,12 @@ import { api } from "@ki-admin-web/backend/convex/_generated/api";
 import { createFileRoute } from "@tanstack/react-router";
 import { zodValidator } from "@tanstack/zod-adapter";
 import { Authenticated, AuthLoading, useQuery } from "convex/react";
-import DashboardAdmin from "@/components/dashboardAdmin";
-import DashboardUser from "@/components/dashboardUser";
+import { lazy } from "react";
 import Loader from "@/components/loader";
 import { dashboardSchema } from "@/lib/utils";
+
+const Admin = lazy(() => import("@/components/dashboardAdmin"));
+const User = lazy(() => import("@/components/dashboardUser"));
 
 export const Route = createFileRoute("/_auth/_layout/dashboard")({
   component: RouteComponent,
@@ -17,9 +19,9 @@ const routeUser = () => {
   const search = Route.useSearch();
 
   if ((user as any)?.role === "admin") {
-    return <DashboardAdmin search={search} />;
+    return <Admin search={search} />;
   } else {
-    return <DashboardUser search={search} />;
+    return <User search={search} />;
   }
 };
 
